dec11992c3f5c537802143d4a8ca230fb17b3823
fsrice <harlanlambert@gmail.com>
2024-07-06 16:58:26 +0800

    add 抛物线的简易实现

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script lang="ts" setup>
 // import StaticList from "./components/DynamicList/StaticList.vue";
-import DynamicList from "./components/DynamicList/DynamicList.vue";
+// import DynamicList from "./components/DynamicList/DynamicList.vue";
+import Parabola from "./components/Animation/parabola/Parabola.vue";
 </script>
 
 <template>
   <!-- <static-list /> -->
-  <dynamic-list />
+  <!-- <dynamic-list /> -->
+  <Parabola />
 </template>
---
+++ src/components/Animation/parabola/Parabola.vue
@@ -0,0 +1,46 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script lang="ts" setup></script>
+
+<template>
+  <!-- 我们要让inner做抛物线运动 -->
+  <div class="outer">
+    <div class="inner"></div>
+  </div>
+</template>
+
+<style scoped>
+.outer {
+  position: absolute;
+  /* 随便定一个位置 确保可以看见整个抛物线 */
+  border-radius: 50%;
+  /* 可以看到外面的outer是匀速直线运动 */
+  border: red dashed 1px;
+  top: 100px;
+  left: 100px;
+  animation: moveX 5s linear forwards;
+}
+.inner {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  background-color: #bfa;
+  animation: moveY 5s cubic-bezier(0, -0.4, 0.81, -0.18) forwards;
+}
+/* 定义两个运动 */
+@keyframes moveX {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(500px);
+  }
+}
+@keyframes moveY {
+  0% {
+    transform: translateY(0);
+  }
+  100% {
+    transform: translateY(500px);
+  }
+}
+</style>
